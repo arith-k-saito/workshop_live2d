5a572c57b5986706caa3d117f05e001ae7e35849
arith-Harrell <m.harrell@arith-metic.jp>
2016-11-18 14:20:20 +0900

    #11 more elements added

---
+++ www/header.php
@@ -0,0 +1,10 @@
+<?php
+
+	echo '
+
+	<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css">
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+	<script src="https://code.jquery.com/jquery-1.9.1.min.js"></script> <!--1.9.1　以上はjquery-mobileが使えなくなります。-->';
+
+
+	?>
