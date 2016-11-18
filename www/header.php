c9ed1acdaee01018b643921ed40765a93aca5b3a
arith-Harrell <m.harrell@arith-metic.jp>
2016-11-18 15:06:11 +0900

    #11 bg

--- www/header.php
+++ www/header.php
@@ -4,7 +4,12 @@
 
 	<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css">
 	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
-	<script src="https://code.jquery.com/jquery-1.9.1.min.js"></script> <!--1.9.1　以上はjquery-mobileが使えなくなります。-->';
+	<script src="https://code.jquery.com/jquery-1.9.1.min.js"></script> <!--1.9.1　以上はjquery-mobileが使えなくなります。-->
+	<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+  	<script src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js"></script>
 
+	<div data-role="header" data-position="fixed">
+	<h1>Fixed Header!</h1>
+	</div>'
 
 	?>
--- www/index.php
+++ www/index.php
@@ -2,4 +2,6 @@
 
     require('header.php');
 
+
     ?>
+
